46b387340fcf282ea5e63a06ced747a7047d433a
Orusa Ishtiaq <orusaishtiaq@gmail.com>
2019-06-30 13:16:41 -0700

    Merging from master

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import './App.css';
+import FormInput from "../Forms/FormInput";
+import Header from "../Forms/header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <FormInput />
     </div>
   );
 }
