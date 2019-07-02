871bf83b45e8ccfc2b63f27b2828b969a1db3a7e
Santosh Kumar <kasantosh@yahoo.com>
2019-07-02 15:10:06 -0400

    initialized and opened all pages

---
+++ client/src/components/layouts/MainPage.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import "./Main.css";
+
+function MainPage() {
+  return (
+    <Fragment>
+      <div class="hero-image d-flex justify-content-center align-items-center">
+        <h1>Parking Lot Manager</h1>
+      </div>
+    </Fragment>
+  );
+}
+
+export default MainPage;
---
+++ client/src/components/layouts/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main.css";
+
+function MainPage() {
+  return (
+    <div>
+      <div class="hero-image d-flex justify-content-center align-items-center">
+        <h1>Sign up</h1>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
