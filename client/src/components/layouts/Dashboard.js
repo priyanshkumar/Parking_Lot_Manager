9a9443fb5d8c3a78f066aea3e319ecddcb712bd1
Santosh Kumar <kasantosh@yahoo.com>
2019-07-03 12:02:56 -0400

    changed the layout for dashboard, hardcoded for now but still comes from state

--- client/src/components/layouts/Dashboard.js
+++ client/src/components/layouts/Dashboard.js
@@ -1,44 +1,117 @@
 import React, { Component } from "react";
-import Spot from "./Spot";
+import Spot1 from "./Spot1";
+import Spot2 from "./Spot2";
+import Spot3 from "./Spot3";
 
 class Dashboard extends Component {
+  state = {
+    Spots1: [
+      { id: "1", spot: "P1" },
+      { id: "2", spot: "P2" },
+      { id: "3", spot: "P3" },
+      { id: "4", spot: "P4" },
+      { id: "5", spot: "P5" },
+      { id: "6", spot: "P6" },
+      { id: "7", spot: "P7" },
+      { id: "8", spot: "P8" },
+      { id: "9", spot: "P9" },
+      { id: "10", spot: "P10" },
+      { id: "11", spot: "P11" },
+      { id: "12", spot: "P12" },
+      { id: "13", spot: "P13" },
+      { id: "14", spot: "P14" },
+      { id: "15", spot: "P15" },
+      { id: "16", spot: "P16" }
+    ],
+    Spots2: [
+      { id: "1", spot: "R1" },
+      { id: "2", spot: "R2" },
+      { id: "3", spot: "R3" },
+      { id: "4", spot: "R4" },
+      { id: "5", spot: "R5" },
+      { id: "6", spot: "R6" },
+      { id: "7", spot: "R7" },
+      { id: "8", spot: "R8" },
+      { id: "9", spot: "R9" },
+      { id: "10", spot: "R10" },
+      { id: "11", spot: "R11" },
+      { id: "12", spot: "R12" },
+      { id: "13", spot: "R13" },
+      { id: "14", spot: "R14" },
+      { id: "15", spot: "R15" },
+      { id: "16", spot: "R16" }
+    ],
+    Spots3: [
+      { id: "1", spot: "S1" },
+      { id: "2", spot: "S2" },
+      { id: "3", spot: "S3" },
+      { id: "4", spot: "S4" },
+      { id: "5", spot: "S5" },
+      { id: "6", spot: "S6" },
+      { id: "7", spot: "S7" },
+      { id: "8", spot: "S8" },
+      { id: "9", spot: "S9" },
+      { id: "10", spot: "S10" },
+      { id: "11", spot: "S11" },
+      { id: "12", spot: "S12" },
+      { id: "13", spot: "S13" },
+      { id: "14", spot: "S14" },
+      { id: "15", spot: "S15" },
+      { id: "16", spot: "S16" }
+    ]
+  };
+
   render() {
     return (
       <div>
         <div className="hero-image d-flex justify-content-center align-items-center">
           <div className="container">
-            <div className="row mb-5">
-              <div className="d-flex col-4">
-                <div className="col-3">
-                  <Spot />
-                </div>
-              </div>
-              <div className="col-4">
-                <Spot />
-              </div>
-              <div className="col-4">
-                <Spot />
-              </div>
-            </div>
-            <div className="container d-flex justify-content-center">
-              <div className="row">
+            {/* Top form begins here */}
+            <div className="">
+              <div className="d-flex justify-content-center row border border-light p-5 mb-5 w-100 bg-light rounded">
                 <form action="">
-                  <div class="form-group text-center font-weight-bold">
-                    <label for="name">PARKING SPOTS SELECTED</label>
+                  <div className="form-group text-center font-weight-bold">
+                    <label htmlFor="name">PARKING SPOTS SELECTED</label>
                     <input
-                      class="form-control"
+                      className="form-control d-inline"
                       type="text"
                       id="selectedSpots"
                       placeholder=""
                     />
                   </div>
-                  <button class="btn btn-warning btn-block" type="submit">
-                    CLEAR SELECTION
-                  </button>
-                  <button class="btn btn-success btn-block" type="submit">
-                    BOOK MY SPOTS
-                  </button>
+                  <div className="d-flex justify-content-center">
+                    <button
+                      className="btn btn-warning d-inline mx-2"
+                      type="submit"
+                    >
+                      CLEAR SELECTION
+                    </button>
+                    <button
+                      className="btn btn-success d-inline mx-2"
+                      type="submit"
+                    >
+                      BOOK MY SPOTS
+                    </button>
+                  </div>
                 </form>
+              </div>
+            </div>
+            {/* Spots arrangement */}
+            <div className="row border border-light bg-light rounded p-3 mb-5">
+              <div className="d-flex flex-wrap justify-content-between col-4">
+                {this.state.Spots1.map(spot => (
+                  <Spot1 key={spot.id} spot={spot.spot} />
+                ))}
+              </div>
+              <div className="d-flex flex-wrap justify-content-between col-4">
+                {this.state.Spots2.map(spot => (
+                  <Spot2 key={spot.id} spot={spot.spot} />
+                ))}
+              </div>
+              <div className="d-flex flex-wrap justify-content-between col-4">
+                {this.state.Spots3.map(spot => (
+                  <Spot3 key={spot.id} spot={spot.spot} />
+                ))}
               </div>
             </div>
           </div>
---
+++ client/src/components/layouts/Spot3.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Spot3(props) {
+  return (
+    <div className="d-flex">
+      <button className="btn btn-danger p-2 m-2">{props.spot}</button>
+    </div>
+  );
+}
+
+export default Spot3;
