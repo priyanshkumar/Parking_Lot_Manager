a6a79b7768a6ed15a8b3acd72ca3a0b52ab4266e
Sunny Heyar <sunny.heyar@gmail.com>
2019-07-06 11:06:49 -0400

    commit for user db

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
+//redirect is not logged on otherwise send to dashboard
+
 router.get("/logout", (req, res) => {
   //handled by passport
 });
