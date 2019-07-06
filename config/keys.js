15dc1b50448d3d83ab66a2bc18a90d003d0674da
Sunny Heyar <sunny.heyar@gmail.com>
2019-07-06 01:28:03 -0400

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  google: {
-    clientID:
-      "341267096007-6c3glni5c2jqm74evh212ol42vnh2arq.apps.googleusercontent.com",
-    clientSecret: "Vsx2iKcR-1K8feLp5q2juILz"
-  }
-};
