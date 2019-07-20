c7a956555b1103cb95ea9e9b5d9ec2caeb3b0af5
Santosh Kumar <kasantosh@yahoo.com>
2019-07-20 13:17:29 -0400

    1. Changed the front page layout - removed the navbar and have one Get Started button that opens the Login auth page 2. Changed Navbar - removed opacity and added logout 3. Created checkout page with table - hardcoded for now 5. Other minor CSS changes

--- client/src/components/clientlayout/mainpage/MainPage.js
+++ client/src/components/clientlayout/mainpage/MainPage.js
@@ -1,23 +1,66 @@
 import React from "react";
 import "./Main.css";
-
-import Navbar from "../navbar/Navbar";
+import logowhite from "./logowhite.png";
 
 function MainPage() {
   return (
     <div>
-      <Navbar />
       <div className="hero-image d-flex flex-column justify-content-start align-items-start">
         <div className="content w-25 p-5 text-center">
           <h2>Welcome</h2>
+          <img src={logowhite} alt="" />
+
           <p className="text-white bg-dark p-3 maintext rounded">
             Our parking lot manages over 600 spots in a convenient location in
             Brampton. If you park regularly at the same location, our discounted
             monthly parking rates may be right for you.
-            <a href="/signup" className="btn btn-success button btn-block mt-2">
+            <a
+              href="/signup"
+              className="btn btn-success button btn-block mt-2"
+              data-toggle="modal"
+              data-target="#openmodal"
+            >
               Get Started
             </a>
           </p>
+        </div>
+        {/* Modal popup below */}
+        <div class="modal" id="openmodal">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header bg-success">
+                <h5 class="modal-title text-white">Login</h5>
+                <button class="close" data-dismiss="modal">
+                  &times;
+                </button>
+              </div>
+              <div class="modal-body">
+                {/* Login auth icons */}
+                <div className="d-flex justify-content-center align-items-center">
+                  <div className="btn-group-vertical">
+                    <a
+                      className="btn btn-block btn-social btn-google"
+                      href="http://localhost:3001/auth/google"
+                    >
+                      <span className="fab fa-google" /> Sign in with Google
+                    </a>
+
+                    <a
+                      className="btn btn-block btn-social btn-github"
+                      href="http://localhost:3001/auth/github"
+                    >
+                      <span className="fab fa-github" /> Sign in with Github
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button class="btn btn-success" data-dismiss="modal">
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
