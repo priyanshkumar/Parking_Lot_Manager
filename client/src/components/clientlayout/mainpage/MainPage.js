fc01590002c0221d2a2968b9918f92404872b0dc
Santosh Kumar <kasantosh@yahoo.com>
2019-07-22 18:35:25 -0400

    made changed to over all layout especially, login and mainpage - changed the fonts for the whole app from index.css

--- client/src/components/clientlayout/mainpage/MainPage.js
+++ client/src/components/clientlayout/mainpage/MainPage.js
@@ -8,8 +8,8 @@ function MainPage() {
     <div>
       <Navbar />
       <div className="hero-image d-flex justify-content-start align-items-center">
-        <div className="content w-50 p-5 text-center">
-          <h1>Welcome</h1>
+        <div className="w-50 p-5 text-center">
+          <h1 class="display-4 text-light">Welcome</h1>
           <h5 className="text-white">
             Our parking lot manages over 600 spots in a convenient location in
             Brampton. <br /> If you park regularly at the same location, our
