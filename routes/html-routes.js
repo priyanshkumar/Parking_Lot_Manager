8cbbd4a866a0596d1b5e2728b2f634234274e353
Sunny Heyar <sunny.heyar@gmail.com>
2019-07-23 18:43:22 -0400

    removed reduntant code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,6 @@
 const path = require("path");
 const router = require("express").Router();
 
-router.get("http://localhost:3000/dashboard", (req, res) => {
-  res.redirect("http://localhost:3000/dashboard");
-  console.log(req.user);
-});
-
 router.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
