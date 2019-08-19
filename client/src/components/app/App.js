f054f900d8ac6cca9a73e880dc5a185b7544ee7a
Priyansh <pradadiya667@gmail.com>
2019-08-19 18:14:35 -0400

    Hash router

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import MainPage from "../clientlayout/mainpage/MainPage";
 import Login from "../clientlayout/login/Login";
 import Signup from "../clientlayout/signup/Signup";
