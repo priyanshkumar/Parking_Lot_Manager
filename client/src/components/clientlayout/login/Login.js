b035c56e78e43021eba2de77f4a284091433a6b9
Priyansh <pradadiya667@gmail.com>
2019-08-31 11:17:47 -0400

    Login and Signup modified with background, font, card, etc.

--- client/src/components/clientlayout/login/Login.js
+++ client/src/components/clientlayout/login/Login.js
@@ -9,24 +9,25 @@ class MainPage extends React.Component {
       <div>
         <Navbar />
         <div className="full-spread d-flex justify-content-center align-items-center">
-          <div className="signupbox border rounded p-5">
-            <h5 className="text-center text-white">
+          <div className="signupbox border rounded px-3 py-4 m-5">
+            <h5 className="text-center">
               Sign in with your Google/Twitter/Facebook Account
             </h5>
-            <hr className="bg-light" />
-            <div className="d-flex justify-content-center mb-3">
-              <a className="btn btn-social btn-google" href="/auth/google">
+            <hr />
+            <div className="d-flex flex-column align-items-center">
+              <a className="btn btn-social btn-google mb-2" href="/auth/google">
                 <span className="fab fa-google" /> Sign in with Google
               </a>
-              <br />
               <a
-                className="btn btn-social btn-twitter mx-3"
+                className="btn btn-social btn-twitter mb-2"
                 href="/auth/twitter"
               >
                 <span className="fab fa-twitter" /> Sign in with Twitter
               </a>
-
-              <a className="btn btn-social btn-facebook" href="/auth/facebook">
+              <a
+                className="btn btn-social btn-facebook mb-2"
+                href="/auth/facebook"
+              >
                 <span className="fab fa-facebook" /> Sign in with Facebook
               </a>
             </div>
